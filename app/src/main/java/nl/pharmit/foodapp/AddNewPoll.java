6b8cafa03fc5e6fdb65c70d672d84639af2c8f77
KeiKKKo <a.sava@student.tue.nl>
2016-07-20 12:04:20 +0200

    Merge branch 'master' of https://github.com/mgmverburg/foodapp
    # Conflicts:
    #	app/src/main/java/nl/pharmit/foodapp/GroupPageCreated.java

--- app/src/main/java/nl/pharmit/foodapp/AddNewPoll.java
+++ app/src/main/java/nl/pharmit/foodapp/AddNewPoll.java
@@ -1,8 +1,9 @@
 package nl.pharmit.foodapp;
 
-import android.content.Intent;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
@@ -12,12 +13,32 @@ import android.widget.ImageButton;
  */
 public class AddNewPoll extends AppCompatActivity {
     ImageButton add;
+    View view;
+
+
 
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin_poll_creating_page);
         ImageButton add = (ImageButton) findViewById(R.id.imageButton2);
+
+    }
+
+    public void thePoll(View view) {
+        Fragment poll = null;
+
+        if(view == findViewById(R.id.add)) {
+            poll = new FragmentPoll();
+
+        }
+        FragmentManager fragmentManager = getFragmentManager();
+        android.app.FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+
+
 
 
     }
