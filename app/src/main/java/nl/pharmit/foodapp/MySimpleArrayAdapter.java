82e2e72914b9cf742b1a318e45a0cdf5e9a35d5f
mgmverburg <m.g.m.verburg@student.tue.nl>
2016-07-21 10:03:33 +0200

    Made listview work

--- app/src/main/java/nl/pharmit/foodapp/MySimpleArrayAdapter.java
+++ app/src/main/java/nl/pharmit/foodapp/MySimpleArrayAdapter.java
@@ -1,6 +1,7 @@
 package nl.pharmit.foodapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,20 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MySimpleArrayAdapter extends ArrayAdapter<String> {
     private final Context context;
@@ -26,6 +41,12 @@ public class MySimpleArrayAdapter extends ArrayAdapter<String> {
         View rowView = inflater.inflate(R.layout.rowlayout, parent, false);
         TextView textView = (TextView) rowView.findViewById(R.id.label);
         Button button = (Button) rowView.findViewById(R.id.remove_user);
+        button.setOnClickListener(new View.OnClickListener() {
+                                      @Override
+                                      public void onClick(View v) {
+
+                                      }
+                                  });
         textView.setText(values[position]);
         // Change the icon for Windows and iPhone
         String s = values[position];
@@ -41,5 +62,54 @@ public class MySimpleArrayAdapter extends ArrayAdapter<String> {
 //        }
 
         return rowView;
+    }
+
+    private void deleteUser(String username, String groupID) {
+        final String paramUsername = username;
+        final String paramGroupID = groupID;
+//        sharedPreferences = getSharedPreferences(getResources().getString(R.string.session), Context.MODE_PRIVATE);
+        //making HTTP request
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, context.getResources().getString(R.string.rootURL)
+                + context.getResources().getString(R.string.removeUser) ,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        JSONObject jObj = null;
+                        String message = "";
+                        Boolean isError = false;
+                        try {
+                            jObj = new JSONObject(response);
+                            isError = jObj.getBoolean("isError");
+                            if (!isError) {
+                                message = jObj.getString(context.getResources().getString(R.string.successMessage));
+                                Toast.makeText(context, message , Toast.LENGTH_LONG).show();
+                                //reload page with data
+                            } else {
+                                Toast.makeText(context, jObj.getString(context.getResources().getString(R.string.successMessage)), Toast.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(context, error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put(context.getResources().getString(R.string.USERNAME), paramUsername);
+                params.put(context.getResources().getString(R.string.GROUPID), paramGroupID);
+                return params;
+            }
+
+        };
+
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(stringRequest);
     }
 }
