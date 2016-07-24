98fe425b35b2c8aea0634079e95787e79c8ded4e
KeiKKKo <a.sava@student.tue.nl>
2016-07-24 11:10:32 +0200

    Merge branch 'master' of https://github.com/mgmverburg/foodapp
    # Conflicts:
    #	app/src/main/java/nl/pharmit/foodapp/GroupPageCreated.java

---
+++ app/src/main/java/nl/pharmit/foodapp/PollPage.java
@@ -0,0 +1,16 @@
+package nl.pharmit.foodapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by s157218 on 21-7-2016.
+ */
+public class PollPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.food_type_choice);
+    }
+}
