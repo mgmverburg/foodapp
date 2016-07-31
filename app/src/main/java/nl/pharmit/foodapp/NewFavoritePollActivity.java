5620b90c931e415376570f5aa4bab1f9d386e3e3
KeiKKKo <a.sava@student.tue.nl>
2016-07-31 16:04:10 +0200

    Favorite Poll

---
+++ app/src/main/java/nl/pharmit/foodapp/NewFavoritePollActivity.java
@@ -0,0 +1,15 @@
+package nl.pharmit.foodapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by s157218 on 31-7-2016.
+ */
+public class NewFavoritePollActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_favorite_poll);
+    }
+}
